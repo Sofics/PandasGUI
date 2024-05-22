7a4ceb7362798679d58bd8a506d96a21df0b4466
Robin Boone <rboone@sofics.com>
2024-05-22 15:14:13 +0200

    compression /w upx working ; don't compress qt files

--- scripts/build.py
+++ scripts/build.py
@@ -45,19 +45,13 @@ command_args += [
     "--noconfirm",
 ]
 
-# TODO get upx to work... way too big otherwise
-# subprocess.CalledProcessError: Command '['./scripts/upx\\upx', '--compress-icons=0', '--lzma', '-q', '--strip-loadconf', 'C:\\Users\\rboone\\AppData\\Local\\pyinstaller\\bincache01py31264bit\\_uuid.pyd']' returned non-zero exit status 1.
-# upx: C:\Users\rboone\AppData\Local\pyinstaller\bincache01py31264bit\_uuid.pyd: NotCompressibleException
-# subprocess.CalledProcessError: Command '['./scripts/upx\\upx', '--compress-icons=0', '--lzma', '-q', '--strip-loadconf', 'C:\\Users\\rboone\\AppData\\Local\\pyinstaller\\bincache01py31264bit\\python3.dll']
-# upx: C:\Users\rboone\AppData\Local\pyinstaller\bincache01py31264bit\python3.dll: NotCompressibleException
-# TODO subprocess.CalledProcessError: Command '['./scripts/upx\\upx', '--compress-icons=0', '--lzma', '-q', '--strip-loadconf', 'C:\\Users\\rboone\\AppData\\Local\\pyinstaller\\bincache01py31264bit\\api-ms-win-core-file-l2-1-0.dll']' returned non-zero exit status 1.
-# TODO upx: C:\Users\rboone\AppData\Local\pyinstaller\bincache01py31264bit\api-ms-win-core-file-l2-1-0.dll: NotCompressibleException
-# if real_build:
-#     # command_args.append("--windowed")  # Not using this option to avoid SVN pop-ups + show potential exceptions.
-#     command_args.append("--upx-dir=./scripts/upx")
-#     # TODO check if _greenlet.cp311-win_amd64.pyd is actually problematic
-#     command_args.append("--upx-exclude=_greenlet.*.pyd")
-
+if real_build:
+    # command_args.append("--windowed")  # Not using this option to avoid SVN pop-ups + show potential exceptions.
+    command_args.append("--upx-dir=./scripts/upx")
+    command_args.append("--upx-exclude=python3.dll")
+    command_args.append("--upx-exclude=api-ms-win-core-file-l2-1-0.dll")
+    command_args.append("--upx-exclude=_uuid.pyd")
+    command_args.append("--upx-exclude=*qt*")
 
 command_args.append("main.py")
 
