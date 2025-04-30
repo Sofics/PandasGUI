f0d26662d97ea35f9040bf65732b14f578160d52
Robin Boone <rboone@sofics.com>
2025-04-30 11:05:49 +0200

    daily id2ip extra std filter date == today

--- pandasgui/store.py
+++ pandasgui/store.py
@@ -910,6 +910,7 @@ class PandasGuiStore:
 
         elif name == "id2ip" and len(self.selected_pgdf.filters) == 0:
             standard_filter_expressions = [
+                f'date == "{datetime.today().strftime("%Y-%m-%d")}"',
                 'date > "2022-03-01"',
                 'type == "TechnicalInformation"',
                 'type_id == 1247 and type == "TechnicalInformation"',
